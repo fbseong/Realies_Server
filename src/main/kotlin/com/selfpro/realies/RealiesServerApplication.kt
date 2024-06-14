5b872e8172264d7bfc28f655fe70600db845daaf
fbseong <fbseong@gmail.com>
2024-06-14 11:58:51 +0900

    :: init project

---
+++ src/main/kotlin/com/selfpro/realies/RealiesServerApplication.kt
@@ -0,0 +1,11 @@
+package com.selfpro.realies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealiesServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealiesServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/selfpro/realies/RealiesServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.selfpro.realies
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealiesServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
